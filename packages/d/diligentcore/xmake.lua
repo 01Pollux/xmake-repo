118db8b207ef81e78a2087328541b53248530b6c
JassJam <0139ne@gmail.com>
2026-06-28 17:34:51 +0200

    add libffi

--- packages/d/diligentcore/xmake.lua
+++ packages/d/diligentcore/xmake.lua
@@ -48,7 +48,7 @@ package("diligentcore")
     add_deps("xxhash")
 
     if is_plat("linux") then
-        add_deps("libx11", "libxrandr", "libxrender", "libxinerama", "libxfixes", "libxcursor", "libxi", "libxext", "wayland")
+        add_deps("libx11", "libxrandr", "libxrender", "libxinerama", "libxfixes", "libxcursor", "libxi", "libxext", "wayland", "libffi")
     end
 
     on_load(function (package)
